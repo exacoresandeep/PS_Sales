e79243f508c6bc05382a45f9163fe2077fef76a8
ansiya786 <ansiyaummmer@gmail.com>
2025-02-18 15:48:20 +0530

    order update

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -286,7 +286,7 @@ class OrderController extends Controller
 
             $orders = $ordersQuery->get()->map(function ($order) {
                 if ($order->created_at) {
-                    $order->created_at = Carbon::createFromFormat('Y-m-d\TH:i:s.u\Z', $order->created_at)->format('d-m-Y');
+                    $order->created_at = Carbon::parse($order->created_at)->format('d-m-Y');
                 }
                 return $order;
             });
